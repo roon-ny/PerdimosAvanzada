d0c399824a04f38750581772fca7d07e1ae386cd
Kevin <ketizam2007@outlook.com>
2026-03-10 20:34:56 -0500

    agregoclasesocio

---
+++ src/tallerminipigs/socioquepasa.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tallerminipigs;
+
+/**
+ *
+ * @author ketiz
+ */
+public class socioquepasa {
+    public void coso(){
+        System.out.println("ajdgjgda");
+    }
+}
