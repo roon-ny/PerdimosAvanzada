51616b887ef7b35303885317d8ffca863c296ea1
roonn <roonn@PC-ROON_NY>
2026-03-10 19:35:58 -0500

    Prueba Minipigs

--- src/tallerminipigs/TallerMinipigs.java
+++ src/tallerminipigs/TallerMinipigs.java
@@ -15,6 +15,7 @@ public class TallerMinipigs {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("kevin bobo");
     }
     
 }
